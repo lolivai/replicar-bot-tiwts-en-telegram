577e7e67083099a3485592631ff0bda4b829d4de
lolivai <103974102+lolivai@users.noreply.github.com>
2023-02-10 23:40:14 -0500

    Create secret.py

---
+++ secret.py
@@ -0,0 +1,6 @@
+# fill these and uncomment
+env = dict(
+         TELEGRAM_BOT_TOKEN="5684862434:AAHSFqKy7bVtyr2SCwNirqYctfYDnxEk9Ug",
+         TWITTER_CONSUMER_KEY="boXxzVDrzDdkZ34AjXANnnIMP",
+         TWITTER_CONSUMER_SECRET="QMBjTUMIvJoJReisYdLfO1FMSUKvO5uPt1xynhJ3Pkrbu8z07s",
+)
